31fdee5f987c3593f0bc9c88b39824b5942545cb
Begun73 <teoss17@gmail.com>
2021-01-14 23:27:26 +0400

    navigation bar(feature)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home/';
 import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
-import ThemeSwitcher from './components/ThemeSwitcher'
+import ThemeSwitcher from './components/ThemeSwitcher';
+import NavigationBar from './components/NavigationBar';
+import { Navigation } from '@material-ui/icons';
 
 const themes = {
   light: `${process.env.PUBLIC_URL}/style/light.css`,
@@ -14,8 +16,9 @@ const themes = {
 const App: React.FC = () => {
   return (
     <ThemeSwitcherProvider defaultTheme="light" themeMap={themes}>
-      <ThemeSwitcher />
-      <Container>
+      <Container maxWidth={false} disableGutters={true}>
+        <ThemeSwitcher />
+        <NavigationBar />
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
